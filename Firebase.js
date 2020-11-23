46bcf61836fd200e5ea72f69fae68bb4aa1feded
ajjiitt <ajitdakre05@gmail.com>
2020-11-23 22:21:44 +0530

    header__23NOV

---
+++ Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyChiZcqGLiw9Ee0ijt05k-ujJ1CUyZqcqQ",
+    authDomain: "clone-cp-ca58a.firebaseapp.com",
+    databaseURL: "https://clone-cp-ca58a.firebaseio.com",
+    projectId: "clone-cp-ca58a",
+    storageBucket: "clone-cp-ca58a.appspot.com",
+    messagingSenderId: "743568952134",
+    appId: "1:743568952134:web:e05796c874950e34ce7347",
+    measurementId: "G-XM80X8KW53"
+  };
---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+function Header() {
+  return (
+    <div className="Header">
+      <img
+        className="header__logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG25.png"
+        alt="AmazonLogo"
+      />
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign in</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingCartIcon className="basket__logo" />
+          <span className=" header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
